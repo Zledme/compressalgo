f449cd38dd79abce317eb0a87eed0b6d289adb8d
zled <zledme@tutanota.com>
2022-08-29 13:59:12 +0530

    end maybe

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from collections import defaultdict
 from maketree import node
-sample_input = "Hey, I just trying to decide what to eat."
+#sample_input = "Hey, I just trying to decide what to eat."
 
-para = sample_input
+para = input()
 freq = defaultdict(int)
 
 for ch in para:
@@ -28,6 +28,7 @@ answerKey = {}
 def getnodes(ri,value):
     if type(ri[0]) == str:
         print(ri[0], value)
+        answerKey[ri[0]] = value
         return 
     if ri[0].left != None:
         getnodes(ri[0].left, value+"0")
@@ -35,3 +36,25 @@ def getnodes(ri,value):
         getnodes(ri[0].right, value+"1")
 
 getnodes(nodes[0], "")
+
+with open('easy.txt', 'w') as file:
+    for ch in para:
+        file.write(answerKey[ch])
+
+answer = ""
+
+def getanswer(f):
+    with open(f,'r') as file:
+        root = nodes[0]
+        for lines in file:
+            for i in lines:
+                if i == "0":
+                    root = root[0].left
+                    #print("left")
+                elif i == "1":
+                    root = root[0].right
+                    #print("right")
+                if(type(root[0]) == str):
+                    print(root[0],end="")
+                    root = nodes[0]
+getanswer("easy.txt")
